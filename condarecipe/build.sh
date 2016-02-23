d1458fe81fd63310ea33d8345938f1e1b5bcae2f
Graham Markall <graham.markall@continuum.io>
2016-02-23 09:02:35 +0000

    Create env var scripts to set path

--- condarecipe/build.sh
+++ condarecipe/build.sh
@@ -4,11 +4,20 @@ export MICROPY_STANDALONE=1
 export MICROPY_PY_UPIP=1
 
 function build() {
+  # Build/install Micropython
   cd unix;
   make deplibs;
   make;
   mkdir $PREFIX/bin;
   cp micropython $PREFIX/bin;
+
+  # Create scripts to set micropython path on environment activation
+  ACTIVATEDIR=$PREFIX/etc/conda/activate.d
+  DEACTIVATEDIR=$PREFIX/etc/conda/deactivate.d
+  mkdir -p $ACTIVATEDIR;
+  mkdir -p $DEACTIVATEDIR;
+  echo -e "#\x21/bin/bash\nLOC=$PREFIX\nexport MICROPYPATH=\$LOC/lib" > $ACTIVATEDIR/env_vars.sh;
+  echo -e "#\x21/bin/bash\nunset MICROPYPATH" > $DEACTIVATEDIR/env_vars.sh;
 }
 
 build
